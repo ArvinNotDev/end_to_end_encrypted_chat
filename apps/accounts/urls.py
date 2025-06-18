01d8d70db1eead10cef2674b1047d4c063a0be92
ArvinNotDev <arveenjafari19@gmail.com>
2025-06-18 17:56:07 +0330

    url added for UploadPublicKeyView

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', UserListView.as_view(), name='user_list'),
-    path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail')
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('users/upload-public-key/', UploadPublicKeyView.as_view(), name='upload_public_key'),  # ✅ Added
 ]
