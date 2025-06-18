6e2c71bd4e3438b7726459e4d16ed5bc0fec862a
ArvinNotDev <arveenjafari19@gmail.com>
2025-06-18 16:16:36 +0330

    login refresh token added

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', UserListView.as_view(), name='user_list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail')
 ]
